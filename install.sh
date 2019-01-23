5a02ba12fc6ca25a0d1fabedda7560270b948a13
jibo <jibo@outlook.com>
2019-01-23 10:34:41 +0800

    Identify vip server

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+VIP=$(echo $1 | tr [A-Z] [a-z])
+echo "identify ${VIP:-free} server"
+
 SS_VERSION=$(curl -sL https://raw.githubusercontent.com/syncxplus/shadowbox2/master/cfg/system.ini | grep SS_VERSION)
+
+VERSION=${SS_VERSION##*=}
 
 docker ps -a | grep -e shadowbox -e inline | awk '{print $1}' | xargs docker rm -f -v
 
-docker rmi syncxplus/shadowbox2:${SS_VERSION##*=}
-
 set -e
-docker run --name shadowbox --restart always -d -e EXCLUSIVE=true --net host -v $PWD/shadowbox:/var/www/shadowsocks/cfg syncxplus/shadowbox2:${SS_VERSION##*=}
+if [[ -z "${VIP}" || "${VIP}" != "vip" ]]; then
+  docker run --name shadowbox --restart always -d --net host -v $PWD/shadowbox:/var/www/shadowsocks/cfg syncxplus/shadowbox2:${VERSION}
+else
+  docker run --name shadowbox --restart always -d -e EXCLUSIVE=true --net host -v $PWD/shadowbox:/var/www/shadowsocks/cfg syncxplus/shadowbox2:${VERSION}
+fi
