f501d36bf4e09b52d395fa2c162945a2df745d34
jibo <jibo@outlook.com>
2019-01-31 17:20:09 +0800

    Enable ss verbose log

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ php /var/www/init.php
 
 SS_DIR=/var/www/shadowsocks
 
-su - www-data -c "ulimit -n 1048576 && ${SS_DIR}/outline-ss-server -metrics 0.0.0.0:9090 -config ${SS_DIR}/cfg/config.yml"
+su - www-data -c "ulimit -n 1048576 && ${SS_DIR}/outline-ss-server -verbose -metrics 0.0.0.0:9090 -config ${SS_DIR}/cfg/config.yml"
