bdc2def9dc1cd17642c8d4e2ead33b1d8e5c6665
jibo <jibo@outlook.com>
2019-02-14 11:24:35 +0800

    Revert "Identify vip server"
    This reverts commit 5a02ba12fc6ca25a0d1fabedda7560270b948a13.

--- install.sh
+++ install.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-VIP=$(echo $1 | tr [A-Z] [a-z])
-echo "identify ${VIP:-free} server"
-
-VERSION=$(curl -sL https://raw.githubusercontent.com/syncxplus/shadowbox2/master/html/version)
-
-docker ps -a | grep -e shadowbox -e inline | awk '{print $1}' | xargs docker rm -f -v
+docker ps -a | grep shadowbox | awk '{print $1}' | xargs -I {} docker rm -f -v {}
+docker images -a | grep shadowbox | awk '{print $3}' | xargs -I {} docker rmi {}
 
 set -e
-if [[ -z "${VIP}" || "${VIP}" != "vip" ]]; then
-  docker run --name shadowbox --restart always -d --net host -v $PWD/shadowbox:/var/www/shadowsocks/cfg syncxplus/shadowbox2:${VERSION}
-else
-  docker run --name shadowbox --restart always -d -e EXCLUSIVE=true --net host -v $PWD/shadowbox:/var/www/shadowsocks/cfg syncxplus/shadowbox2:${VERSION}
-fi
+
+VERSION=$(curl -sL https://raw.githubusercontent.com/syncxplus/shadowbox2/master/html/version)
+docker run --name shadowbox --restart always -d -e EXCLUSIVE=true --net host -v $PWD/shadowbox:/var/www/shadowsocks/cfg syncxplus/shadowbox2:${VERSION}
